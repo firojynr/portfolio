469dca19ed9eb304ba247f1a25c93a18b0ffef5e
Firoj Khan <139542859+firojynr@users.noreply.github.com>
2024-04-02 15:06:14 +0530

    Add files via upload

---
+++ project.js
@@ -0,0 +1,224 @@
+function fn1(){
+
+    document.getElementById("1").innerText = "2";
+    document.getElementById("2").innerText = "5";
+    document.getElementById("3").innerText = "8";
+    document.getElementById("4").innerText = "11";
+    document.getElementById("5").innerText = "14";
+    document.getElementById("6").innerText = "17";
+    document.getElementById("7").innerText = "20";
+    document.getElementById("8").innerText = "1";
+    document.getElementById("9").innerText = "4";
+    document.getElementById("10").innerText = "7";
+    document.getElementById("11").innerText = "10";
+    document.getElementById("12").innerText = "13";
+    document.getElementById("13").innerText = "16";
+    document.getElementById("14").innerText = "19";
+    document.getElementById("15").innerText = "3";
+    document.getElementById("16").innerText = "6";
+    document.getElementById("17").innerText = "9";
+    document.getElementById("18").innerText = "12";
+    document.getElementById("19").innerText = "15";
+    document.getElementById("20").innerText = "18";
+    document.getElementById("21").innerText = "21";
+    
+}
+
+function fn2(){
+    document.getElementById("1").innerText = "1";
+    document.getElementById("2").innerText = "4";
+    document.getElementById("3").innerText = "7";
+    document.getElementById("4").innerText = "10";
+    document.getElementById("5").innerText = "13";
+    document.getElementById("6").innerText = "16";
+    document.getElementById("7").innerText = "19";
+    document.getElementById("8").innerText = "2";
+    document.getElementById("9").innerText = "5";
+    document.getElementById("10").innerText = "8";
+    document.getElementById("11").innerText = "11";
+    document.getElementById("12").innerText = "14";
+    document.getElementById("13").innerText = "17";
+    document.getElementById("14").innerText = "20";
+    document.getElementById("15").innerText = "3";
+    document.getElementById("16").innerText = "6";
+    document.getElementById("17").innerText = "9";
+    document.getElementById("18").innerText = "12";
+    document.getElementById("19").innerText = "15";
+    document.getElementById("20").innerText = "18";
+    document.getElementById("21").innerText = "21";
+    
+}
+function fn3(){
+    document.getElementById("1").innerText = "1";
+    document.getElementById("2").innerText = "4";
+    document.getElementById("3").innerText = "7";
+    document.getElementById("4").innerText = "10";
+    document.getElementById("5").innerText = "13";
+    document.getElementById("6").innerText = "16";
+    document.getElementById("7").innerText = "19";
+    document.getElementById("8").innerText = "3";
+    document.getElementById("9").innerText = "6";
+    document.getElementById("10").innerText = "9";
+    document.getElementById("11").innerText = "12";
+    document.getElementById("12").innerText = "15";
+    document.getElementById("13").innerText = "18";
+    document.getElementById("14").innerText = "21";
+    document.getElementById("15").innerText = "2";
+    document.getElementById("16").innerText = "5";
+    document.getElementById("17").innerText = "8";
+    document.getElementById("18").innerText = "11";
+    document.getElementById("19").innerText = "14";
+    document.getElementById("20").innerText = "17";
+    document.getElementById("21").innerText = "20";
+
+}
+
+
+
+
+
+
+
+function afn1(){
+    
+        document.getElementById("1").innerText = "5";
+        document.getElementById("2").innerText = "14";
+        document.getElementById("3").innerText = "1";
+        document.getElementById("4").innerText = "10";
+        document.getElementById("5").innerText = "19";
+        document.getElementById("6").innerText = "9";
+        document.getElementById("7").innerText = "18";
+        document.getElementById("8").innerText = "2";
+        document.getElementById("9").innerText = "11";
+        document.getElementById("10").innerText = "20";
+        document.getElementById("11").innerText = "7";
+        document.getElementById("12").innerText = "16";
+        document.getElementById("13").innerText = "6";
+        document.getElementById("14").innerText = "15";
+        document.getElementById("15").innerText = "8";
+        document.getElementById("16").innerText = "17";
+        document.getElementById("17").innerText = "4";
+        document.getElementById("18").innerText = "13";
+        document.getElementById("19").innerText = "3";
+        document.getElementById("20").innerText = "12";
+        document.getElementById("21").innerText = "21";
+
+    } 
+    
+    function afn2(){
+        document.getElementById("1").innerText = "2";
+        document.getElementById("2").innerText = "11";
+        document.getElementById("3").innerText = "20";
+        document.getElementById("4").innerText = "7";
+        document.getElementById("5").innerText = "16";
+        document.getElementById("6").innerText = "6";
+        document.getElementById("7").innerText = "15";
+        document.getElementById("8").innerText = "5";
+        document.getElementById("9").innerText = "14";
+        document.getElementById("10").innerText = "1";
+        document.getElementById("11").innerText = "10";
+        document.getElementById("12").innerText = "19";
+        document.getElementById("13").innerText = "9";
+        document.getElementById("14").innerText = "18";
+        document.getElementById("15").innerText = "8";
+        document.getElementById("16").innerText = "17";
+        document.getElementById("17").innerText = "4";
+        document.getElementById("18").innerText = "13";
+        document.getElementById("19").innerText = "3";
+        document.getElementById("20").innerText = "12";
+        document.getElementById("21").innerText = "21";
+
+    } 
+    
+    function afn3(){
+
+        document.getElementById("abt3").style.backgroundColor="yellow";
+        document.getElementById("1").innerText = "2";
+        document.getElementById("2").innerText = "11";
+        document.getElementById("3").innerText = "20";
+        document.getElementById("4").innerText = "7";
+        document.getElementById("5").innerText = "16";
+        document.getElementById("6").innerText = "6";
+        document.getElementById("7").innerText = "15";
+        document.getElementById("8").innerText = "8";
+        document.getElementById("9").innerText = "17";
+        document.getElementById("10").innerText = "4";
+        document.getElementById("11").innerText = "13";
+        document.getElementById("12").innerText = "3";
+        document.getElementById("13").innerText = "12";
+        document.getElementById("14").innerText = "21";
+        document.getElementById("15").innerText = "5";
+        document.getElementById("16").innerText = "14";
+        document.getElementById("17").innerText = "1";
+        document.getElementById("18").innerText = "10";
+        document.getElementById("19").innerText = "19";
+        document.getElementById("20").innerText = "9";
+        document.getElementById("21").innerText = "18";
+    }
+
+
+
+
+
+
+
+
+
+function getdata(){
+
+    var val=document.getElementById("inp").value;
+
+    if
+    
+}
+
+
+
+
+function start()
+{
+    var n=prompt("Enter Row No: ");
+
+    if (n==1)
+    {
+        fn1();
+
+        
+
+
+
+
+
+    } else if (n==2)
+    {
+        fn2();
+    } else if (n==3)
+    {
+        fn3();
+    }
+}
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
